61c9699887e4b3f29670adcf82244814c7507e57
yamolean <moririnmanro9608@gmail.com>
2020-03-05 05:23:06 +0900

    first commit

---
+++ ExRx/BottomReachedTrigger.swift
@@ -0,0 +1,9 @@
+//
+//  BottomReachedTrigger.swift
+//  ExRx
+//
+//  Created by 矢守叡 on 2020/03/05.
+//  Copyright © 2020 yamolean. All rights reserved.
+//
+
+import Foundation
---
+++ ExRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExRx
+//
+//  Created by 矢守叡 on 2020/03/05.
+//  Copyright © 2020 yamolean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
